e6191fc8cc6da592fd3eec8ce2b25aced783a683
Matthias Fuchs <matthias.fuchs@kps.com>
2023-04-26 21:23:09 +0200

    fixed setup.sh

--- services/plugwiseconnect/setup.sh
+++ services/plugwiseconnect/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pip3 install requests crcmod async_timeout defusedxml pyserial python-dateutil semver serial sqlite_utils
+pip3 install requests crcmod async_timeout defusedxml serial pyserial python-dateutil semver serial sqlite_utils
