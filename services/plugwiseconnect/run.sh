02164997ee638d5c32c135a7a934aed914b399a3
Matthias Fuchs <matthias.fuchs@kps.com>
2023-05-22 14:23:32 +0200

    refactoring

--- services/plugwiseconnect/run.sh
+++ services/plugwiseconnect/run.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # kill old instance
-(cd $SCRIPT_DIR; kill $(cat .pid)
+(cd $SCRIPT_DIR; kill $(cat .pid))
 
 (cd $SCRIPT_DIR; python3 __main__.py --config ./config.json)
 
