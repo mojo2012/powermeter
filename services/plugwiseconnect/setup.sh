988fe44d4bd32405c157567ba450be877e13bfa0
Matthias Fuchs <matthias.fuchs@kps.com>
2023-09-13 08:35:11 +0200

    changed setup.sh

--- services/plugwiseconnect/setup.sh
+++ services/plugwiseconnect/setup.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-pip3 install plugwise krak paho-mqtt requests crcmod async_timeout defusedxml serial pyserial python-dateutil semver serial sqlite_utils pyW215 xmltodict
+pip3 install
+pip3 plugwise
+pip3 requests
+pip3 crcmod
+pip3 async_timeout
+pip3 defusedxml
+pip3 serial
+pip3 pyserial
+pip3 python-dateutil
+pip3 semver
+pip3 serial
+pip3 pyW215
+pip3 xmltodict
+pip3 krak
+pip3 paho-mqtt
+pip3 sqlite_utils
